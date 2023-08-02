2940c6c1f3579cb94f6d137666cc797c57d2262c
bernardolucas2001 <bernardosopaulino@gmail.com>
2023-08-02 17:34:04 -0300

    Exercicios

---
+++ OlaMundo/src/principal/OlaMundo.java
@@ -0,0 +1,20 @@
+package principal;
+
+public class OlaMundo {
+
+	public static void main(String[] args) {
+		
+		//imprime uma linha e ao final salta uma nova linha
+		//print = imprimir; in = new line;
+		System.out.println("Olá Mundo, Hello world!");
+		
+		//comentario de uma linha
+		/*
+		 * esse é
+		 * um comentario
+		 * de multiplas linhas
+		 */
+
+	}
+
+}
