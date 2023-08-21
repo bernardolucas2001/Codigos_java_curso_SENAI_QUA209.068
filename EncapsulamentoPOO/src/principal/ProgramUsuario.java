15d4a7f45c9c7c6cdea87f46d221f1d7d0789aba
bernardolucas2001 <bernardosopaulino@gmail.com>
2023-08-21 17:32:29 -0300

    adicionando comentario ao exercicio ProgramUsuario

--- EncapsulamentoPOO/src/principal/ProgramUsuario.java
+++ EncapsulamentoPOO/src/principal/ProgramUsuario.java
@@ -7,7 +7,11 @@ import entities.Usuario;
 public class ProgramUsuario {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//crie um programa que receba o nome, o e-mail e a senha do usuário,
+		//o usuário só poderá alterar a senha através de um método e
+		//a senha não pode ser mostrada na tela principal do programa;
+		
+		
 		Scanner lendo = new Scanner(System.in);
 		Usuario usuario = new Usuario();
 		
