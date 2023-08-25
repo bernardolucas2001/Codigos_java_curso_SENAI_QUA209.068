24c7926c6316b6580d91dd78c32ffee23d0c1f7f
Bernardo <bernardosaopaulino@gmail.com>
2023-08-25 15:28:33 -0300

    pequenos ajustes

--- ListasCollections/src/principal/ProgramProduto.java
+++ ListasCollections/src/principal/ProgramProduto.java
@@ -20,13 +20,19 @@ public class ProgramProduto {
 	    System.out.println("Digite a quantidade do primeiro produto:");
 	    int quantidade1 = scan.nextInt();
 	    
+	    System.out.println("Digita o preço do primeiro produto:");
+	    double preco1 = scan.nextDouble();
+	    
 	   
 		 
-		System.out.println("Digite nome segundo produto:");
+		System.out.println("Digite nome do segundo do produto:");
 		 String nome2 = scan.next();
 		
 		System.out.println("Digite a quantidade do segundo produto:");
 		int quantidade2 = scan.nextInt();
+		
+		System.out.println("Digita o preço do segundo produto:");
+	    double preco2 = scan.nextDouble();
 		 
 		
 		System.out.println("Digite nome do terceiro produto:");
@@ -35,11 +41,14 @@ public class ProgramProduto {
 		System.out.println("Digite a quantidade do terceiro produto:");
 	    int quantidade3 = scan.nextInt();
 	    
+	    System.out.println("Digita o preço do terceiro produto:");
+	    double preco3 = scan.nextDouble();
+	    
 	    
 		
-		Produtos produto1 = new Produtos(nome1, quantidade1, 5.00);
-		Produtos produto2 = new Produtos(nome2, quantidade2, 10.50);
-		Produtos produto3 = new Produtos(nome3, quantidade3, 20.50);
+		Produtos produto1 = new Produtos(nome1, quantidade1, preco1);
+		Produtos produto2 = new Produtos(nome2, quantidade2, preco2);
+		Produtos produto3 = new Produtos(nome3, quantidade3, preco3);
 		
 		
 		
